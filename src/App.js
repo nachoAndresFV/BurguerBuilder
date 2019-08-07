483385e65e9219c6bff56ba17983f9aad3141c1e
Ignacio <ignacio.farias.v@usach.cl>
2019-08-07 14:30:37 -0400

    Layout component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import Layout from './components/layout/Layout';
 function App() {
   return (
     <div>
-      
+      <Layout>
+        <h1>This is working!</h1>
+      </Layout>
     </div>
   );
 }
