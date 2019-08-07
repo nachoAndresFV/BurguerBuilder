37e87f9e7d9dcba9d3dd0ad376322b02249083e5
Ignacio <ignacio.farias.v@usach.cl>
2019-08-07 15:13:52 -0400

    Props Type Validation

--- src/components/Burguer/BurguerIngredient/BurguerIngredient.js
+++ src/components/Burguer/BurguerIngredient/BurguerIngredient.js
@@ -1,35 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import burguerIngredientStyle from './BurguerIngredient.module.css';
+import PropTypes from 'prop-types';
 
-const burguerIngredient = (props) => {
-    let ingredient = null;
-    switch (props.type) {
-        case 'BreadBotton':
-            ingredient = <div className={burguerIngredientStyle.BreadBottom}></div>;
-            break;
-        case 'BreaTop':
-            ingredient = (
-                <div className={burguerIngredientStyle.BreadTop}>
-                    <div className={burguerIngredientStyle.Seeds1}></div>
-                    <div className={burguerIngredientStyle.Seeds2}></div>
-                </div>);
-            break;
-        case 'Meat':
-            ingredient = <div className={burguerIngredientStyle.Meat}></div>;
-            break;   
-        case 'Cheese':
-            ingredient = <div className={burguerIngredientStyle.Cheese}></div>;
-            break;  
-        case 'Salad':
-            ingredient = <div className={burguerIngredientStyle.Salad}></div>;
-            break; 
-        case 'Bacon':
-            ingredient = <div className={burguerIngredientStyle.Bacon}></div>;
-            break; 
-        default:
-            ingredient = null;
+class BurguerIngredient extends Component {
+    render(){
+        let ingredient = null;
+        switch (this.props.type) {
+            case 'BreadBotton':
+                ingredient = <div className={burguerIngredientStyle.BreadBottom}></div>;
+                break;
+            case 'BreaTop':
+                ingredient = (
+                    <div className={burguerIngredientStyle.BreadTop}>
+                        <div className={burguerIngredientStyle.Seeds1}></div>
+                        <div className={burguerIngredientStyle.Seeds2}></div>
+                    </div>);
+                break;
+            case 'Meat':
+                ingredient = <div className={burguerIngredientStyle.Meat}></div>;
+                break;   
+            case 'Cheese':
+                ingredient = <div className={burguerIngredientStyle.Cheese}></div>;
+                break;  
+            case 'Salad':
+                ingredient = <div className={burguerIngredientStyle.Salad}></div>;
+                break; 
+            case 'Bacon':
+                ingredient = <div className={burguerIngredientStyle.Bacon}></div>;
+                break; 
+            default:
+                ingredient = null;
+        }
+        return ingredient;
     }
-    return ingredient;
+    
 };
 
-export default burguerIngredient;
+BurguerIngredient.propTypes = {
+    type: PropTypes.string.isRequired
+}
+
+
+export default BurguerIngredient;
