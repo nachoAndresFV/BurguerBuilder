70f0a71eda8b0434c891e708f9bb12c0892ee421
Ignacio <ignacio.farias.v@usach.cl>
2019-08-14 15:08:55 -0400

    The checkout container

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, {Component} from 'react';
 import Layout from './hoc/layout/Layout';
 import BurguerBuilder from './containers/BurguerBuilder/BurguerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+
 class App extends Component {
-  state = {
-    show: true
-  }
+  // state = {
+  //   show: true
+  // }
   componentDidMount(){
-    setTimeout(()=>{
-      this.setState({
-        show: false
-      })
-    }, 5000);
+    // setTimeout(()=>{
+    //   this.setState({
+    //     show: false
+    //   })
+    // }, 5000);
   }
   render(){
     return (
       <div>
         <Layout>
-          {this.state.show ? <BurguerBuilder/> : null }
+          {/*this.state.show ? <BurguerBuilder/> : null */}
+          <BurguerBuilder/>
+          <Checkout/>
         </Layout>
       </div>
     );
